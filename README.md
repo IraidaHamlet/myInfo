506d9445c5dc160447de04fb1e69b73306360e16
IraidaHamlet <153469394+IraidaHamlet@users.noreply.github.com>
2024-07-06 10:01:41 -0400

    Update and rename README.md to myInfo.py

--- README.md
+++
@@ -1,5 +0,0 @@
-# myInfo
-
-print("My name is Iraida Hamlet")
-print("My address is 22746 NW 142nd Ave")
-print("Phone number is 203-954-7753")
---
+++ myInfo.py
@@ -0,0 +1,11 @@
+# myInfo
+
+
+name = Iraida Hamlet
+address = 22746 NW 142nd Ave
+telephone number = 203-954-7753
+
+
+print("My name is " + "name")
+print("My address is " + "address")
+print("Telepone number is " + "telephone number")
